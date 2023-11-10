2d81b1c226a478f9f49afa81b02f3ae2f6fb69fd
ZhenLu <luzhens@outlook.com>
2023-11-10 08:52:37 -0500

    friday

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,7 +1,12 @@
+import React from 'react';
 function Home() {
     return (
         <div>
             <h1>Hello World in Home page</h1>
+            <h2>Hi 5610 project</h2>
+            <h2>is it working</h2>
+            <p>friday</p>
+
         </div>
     );
 }
--- src/User/index.js
+++ src/User/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 import AccountInfo from './AccountInfo';
 import PaymentMethods from './PaymentMethods';
 import Addresses from './Addresses';
