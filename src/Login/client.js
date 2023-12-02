219fea066dae5e7c27134b4f6efc95194273bd7d
1115yiming <yimingzhuzhu@gmail.com>
2023-12-01 23:25:58 -0500

    login successfully / signup not writing into database

---
+++ src/Login/client.js
@@ -0,0 +1,41 @@
+// src/Login/client.js
+
+const API_BASE_URL = 'http://localhost:4000'; // Replace with your server's URL
+
+export async function registerUser(email, password, role) {
+    try {
+        const response = await fetch(`${API_BASE_URL}/api/users`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password, role })
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to register user');
+        }
+
+        return response.json();
+    } catch (error) {
+        console.error('Error registering user:', error);
+        throw error;
+    }
+}
+
+export async function checkUserExists(email) {
+    try {
+        const response = await fetch(`${API_BASE_URL}/api/users?email=${email}`);
+        // console.log('Response from checkUserExists:', response); // Debugging line
+        if (!response.ok) {
+            throw new Error('Failed to check if user exists');
+        }
+        const users = await response.json();
+        // console.log('Users with the same email:', users); // Debugging line
+        return users.length > 0;
+    } catch (error) {
+        // console.error('Error checking user existence:', error);
+        throw error;
+    }
+}
+
--- src/Login/index.js
+++ src/Login/index.js
@@ -2,71 +2,17 @@ import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import SignupForm from './SignupForm';
 import LoginForm from './LoginForm';
-import './index.css';
 import HorizontalNavigation from '../Home/NavigationBar/index.js';
-
-
-// if (localStorage.getItem('users') === null) {
-//     localStorage.setItem('users', JSON.stringify([
-//                                                      {
-//                                                          "email": "yiming@163.com",
-//                                                          "password": "123456789",
-//                                                          "role": "customer"
-//                                                      },
-//                                                      {
-//                                                          "email": "johnnydepp@gmail.com",
-//                                                          "password": "bequickman",
-//                                                          "role": "delivery person"
-//                                                      },
-//                                                      {
-//                                                          "email": "bestsandwich@outlook.com",
-//                                                          "password": "iwanttoeat",
-//                                                          "role": "merchant"
-//                                                      }
-//                                                  ]));
-// }
+import './index.css';
+import { registerUser, checkUserExists } from './client';
 
 function Login() {
-    // const handleSignup = (email, password, role) => {
-    //     console.log(email, password, role);
-    // };
-    //
-    // const handleLogin = (email, password) => {
-    //     console.log(email, password);
-    // };
 
     const isValidEmail = (email) => {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return emailRegex.test(email);
     };
 
-    const doesEmailExist = (email) => {
-        const users = JSON.parse(localStorage.getItem('users')) || [];
-        return users.some(user => user.email === email);
-    };
-
-    const addUserToDatabase = (email, password, role) => {
-        const users = JSON.parse(localStorage.getItem('users')) || [];
-        users.push({ email, password, role });
-        localStorage.setItem('users', JSON.stringify(users));
-    };
-
-    // const handleSignup = (email, password, confirmPassword, role) => {
-    //     if (!isValidEmail(email)) {
-    //         alert("Please enter a valid email address.");
-    //         return;
-    //     }
-    //     if (doesEmailExist(email)) {
-    //         alert("An account with this email already exists. Please log in.");
-    //         return;
-    //     }
-    //     if (password !== confirmPassword) {
-    //         alert("Passwords don't match.");
-    //         return;
-    //     }
-    //     addUserToDatabase(email, password, role);
-    //     alert("Signup successful!");
-    // };
 
     const handleSignup = async (email, password, confirmPassword, role) => {
         if (!isValidEmail(email)) {
@@ -74,56 +20,28 @@ function Login() {
             return;
         }
 
-        // Check if email already exists (this will now be a request to your server)
-        const response = await fetch(`http://localhost:4000/api/users?email=${email}`);
-        const usersWithSameEmail = await response.json();
+        try {
+            const emailExists = await checkUserExists(email);
+            // console.log(`Does email exist? ${emailExists}`);
+            if (emailExists) {
+                alert("An account with this email already exists. Please log in.");
+                return;
+            }
 
-        if (usersWithSameEmail.length > 0) {
-            alert("An account with this email already exists. Please log in.");
-            return;
-        }
+            // console.log('Password:', password);
+            // console.log('Confirm Password:', confirmPassword);
+            if (password.trim() !== confirmPassword.trim()) {
+                alert("Passwords don't match.");
+                return;
+            }
 
-        if (password !== confirmPassword) {
-            alert("Passwords don't match.");
-            return;
-        }
-
-        // Send a request to the server to create a new user
-        const createUserResponse = await fetch('http://localhost:4000/api/users', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ email, password, role })
-        });
-
-        if (createUserResponse.ok) {
+            await registerUser(email, password, role);
             alert("Signup successful!");
-        } else {
-            alert("Signup failed. Please try again.");
+        } catch (error) {
+            alert("Signup failed: " + error.message);
         }
     };
 
-
-    // const handleLogin = (email, password) => {
-    //     // const users = JSON.parse(localStorage.getItem('users')) || [];
-    //     // const user = users.find(user => user.email === email && user.password === password);
-    //     // Trim and normalize email input for consistent matching
-    //
-    //     const normalizedEmail = email.trim().toLowerCase();
-    //     const users = JSON.parse(localStorage.getItem('users')) || [];
-    //
-    //     // Find a user where the email and password match
-    //     const user = users.find(user =>
-    //                                 user.email.toLowerCase() === normalizedEmail && user.password === password
-    //     );
-    //
-    //     if (user) {
-    //         alert("Login successful!");
-    //     } else {
-    //         alert("Incorrect email or password. Please try again.");
-    //     }
-    // };
 
     const handleLogin = async (email, password) => {
         const normalizedEmail = email.trim().toLowerCase();
@@ -151,12 +69,11 @@ function Login() {
             <div className="main-content">
                 <h1>Welcome to Wollaston's!</h1>
                 <Link to="/login/login" className="button-sign-in">Log in</Link>
-                <Link to="/login/signup" className="link-sign-up">Don't have an account? Sign up now</Link>
+                <Link to="/login/signup" className="link-sign-up">Don't have an account? Register now</Link>
             </div>
             <Routes>
                 <Route path="/signup" element={<SignupForm onSignup={handleSignup} />} />
                 <Route path="/login" element={<LoginForm onLogin={handleLogin} />} />
-                //rendering:  LJSX that represents the LoginForm component
             </Routes>
         </div>
     );
