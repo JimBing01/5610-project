92059ccd7db1a59df902f35c6121c711c34001f6
jinpengchen-cyber <chen.jinp@northeastern.edu>
2023-12-09 18:30:02 -0500

    commit

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,22 +1,37 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import NavigationBar from "./NavigationBar";
 import SandwichSlider from "./Slider/SandwichSlider";
 import Menu from "./Menu";
 import "./index.css";    
 import {BrowserRouter as Router, Routes, Route, useParams,} from 'react-router-dom';
+import {useLocation} from "react-router-dom";
 import User from "../User";
 import SandwichModal from "./Menu/sandwichModal/sandwichModal";
+import * as client from "./client";
 
 
 
 function Home() {
 	const {userId} = useParams();
+	const location = useLocation();
 	const [selectedSandwich, setSelectedSandwich] = useState(null);
+	const [userInfo, setUserInfo] = useState({ firstName: '' }); // Added userInfo state
 
 	const handleSelectSandwich = (sandwich) => {
 		console.log("Selected Sandwich: ", sandwich); // Check if this logs the correct sandwich
 		setSelectedSandwich(sandwich);
 	};
+
+	useEffect(() => {
+		if (userId) {
+			client.getUserById(userId)
+  .then((user) => {
+    setUserInfo(user); // This should set the userInfo state to the user object
+  })
+  .catch(error => console.error('Error fetching user data:', error));
+		}
+	}, [userId]);
+	const displayWelcomeSlogan = location.pathname.includes('/user/'); // Check if URL contains '/user/'
 
     return (
 			<div className="home">
@@ -24,6 +39,10 @@ function Home() {
 					userId={userId}
 					onSelectSandwich={handleSelectSandwich}
 				/>
+				 {displayWelcomeSlogan && userInfo.firstName && (
+  <div className="welcome-slogan">Hello {userInfo.firstName}! Fresh, Delicious Sandwiches Await You</div>
+)}
+
 				{selectedSandwich && (
 					<SandwichModal
 						sandwich={selectedSandwich}
