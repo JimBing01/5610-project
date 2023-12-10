4d07f61383a1b57c7fda96de4e0d9d2dbc969b84
1115yiming <yimingzhuzhu@gmail.com>
2023-12-10 18:40:48 -0500

    login and signup successfully

--- src/Login/SignupForm.js
+++ src/Login/SignupForm.js
@@ -12,18 +12,73 @@ const SignupForm = ({ onSignup }) => {
     const [confirmPassword, setConfirmPassword] = useState('');
     const [role, setRole] = useState('');
 
-    const handleSubmit = (event) => {
+
+    const isValidEmail = (email) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    };
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        if (password !== confirmPassword) {
-            alert("Passwords don't match");
+
+        if (!isValidEmail(email)) {
+            alert("Please enter a valid email address.");
             return;
         }
-        onSignup(username, firstName, lastName, email, dob, phone, password, role);
+
+        if (password !== confirmPassword) {
+            alert("Passwords don't match.");
+            return;
+        }
+
+        const userData = { username, firstName, lastName, email, dob, phone, password, role };
+        onSignup(userData);
     };
+
 
     return (
         <div className="form-container"> {/* Apply the container class */}
             <form onSubmit={handleSubmit}>
+
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="confirm-password">Confirm Password:</label>
+                    <input
+                        type="password"
+                        id="confirm-password"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="role-selection">
+                    <label>Role:</label>
+                    <div className="role-options" onChange={(e) => setRole(e.target.value)}>
+                        <input type="radio" value="customer" name="role" required /> Customer
+                        <input type="radio" value="merchant" name="role" required /> Merchant
+                        <input type="radio" value="delivery" name="role" required /> Delivery
+                    </div>
+                </div>
+
                 <div>
                     <label htmlFor="username">Username:</label>
                     <input
@@ -74,44 +129,7 @@ const SignupForm = ({ onSignup }) => {
                         required
                     />
                 </div>
-                <div>
-                    <label htmlFor="email">Email:</label>
-                    <input
-                        type="email"
-                        id="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="password">Password:</label>
-                    <input
-                        type="password"
-                        id="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="confirm-password">Confirm Password:</label>
-                    <input
-                        type="password"
-                        id="confirm-password"
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className="role-selection">
-                    <label>Role:</label>
-                    <div className="role-options" onChange={(e) => setRole(e.target.value)}>
-                        <input type="radio" value="customer" name="role" required /> Customer
-                        <input type="radio" value="merchant" name="role" required /> Merchant
-                        <input type="radio" value="delivery" name="role" required /> Delivery
-                    </div>
-                </div>
+
                 <button type="submit">Register me</button>
             </form>
         </div>
