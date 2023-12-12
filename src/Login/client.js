1879e729a3008d4c4eff1268ac53b47d605d0d0a
1115yiming <yimingzhuzhu@gmail.com>
2023-12-11 22:06:42 -0500

    api update

--- src/Login/client.js
+++ src/Login/client.js
@@ -1,11 +1,11 @@
 // src/Login/client.js
 
-// const API_BASE_URL = process.env.REACT_APP_BASE_API_URL;
-const API_BASE = process.env.REACT_APP_API_BASE;
+const API_BASE_URL = process.env.REACT_APP_BASE_API_URL;
+// const API_BASE = process.env.REACT_APP_API_BASE;
 
 export async function registerUser(userData) {
     try {
-        const response = await fetch(`${API_BASE}/users/signup`, {
+        const response = await fetch(`${API_BASE_URL}/users/signup`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -28,7 +28,7 @@ export async function registerUser(userData) {
 
 export async function checkUserExists(email) {
     try {
-        const response = await fetch(`${API_BASE}/users?email=${encodeURIComponent(email)}`, {
+        const response = await fetch(`${API_BASE_URL}/users?email=${encodeURIComponent(email)}`, {
             credentials: 'include'
         });
         console.log('Response from checkUserExists:', response);
